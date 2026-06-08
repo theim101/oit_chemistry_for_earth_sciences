626baa969a0e59c9f14ae62059629410c7d2a9d3
Timo Heimovaara <t.j.heimovaara@tudelft.nl>
2026-06-08 16:51:56 +0200

    20260608 worked on travertine example, and buffer examples

---
+++ chemistry_book/content/week 4.2/ExamApril2026/context.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from BioChemicalClass import BiochemRatesClass
+# from BioChemicalClass import stoichio_module
+from BioChemicalClass import PlottingModule
